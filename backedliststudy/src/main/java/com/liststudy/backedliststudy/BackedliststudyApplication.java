3d0992d9e55b67cee985366493be4ec38b353d6a
alber <alber@192.168.1.115>
2018-12-07 12:50:10 +0100

    Inicialización proyecto

---
+++ backedliststudy/src/main/java/com/liststudy/backedliststudy/BackedliststudyApplication.java
@@ -0,0 +1,12 @@
+package com.liststudy.backedliststudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackedliststudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackedliststudyApplication.class, args);
+	}
+}
