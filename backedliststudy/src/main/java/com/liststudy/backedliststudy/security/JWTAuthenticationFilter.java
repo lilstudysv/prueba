71b8476c87e76cebac76bf0ddfff9282f8abd149
alber <lilstudysv@gmal.com>
2019-01-06 17:44:04 +0100

    Improve security for authentication and authorization

--- backedliststudy/src/main/java/com/liststudy/backedliststudy/security/JWTAuthenticationFilter.java
+++ backedliststudy/src/main/java/com/liststudy/backedliststudy/security/JWTAuthenticationFilter.java
@@ -28,7 +28,6 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
 	public static final String TOKEN_BEARER_PREFIX = "Bearer ";
 
-	public static final String ISSUER_INFO = "https://www.autentia.com/";
 	public static final String SUPER_SECRET_KEY = "1234";
 	public static final long TOKEN_EXPIRATION_TIME = 864_000_000; // 10 day
 	
@@ -56,7 +55,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication auth) throws IOException, ServletException {
 
-		String token = Jwts.builder().setIssuedAt(new Date()).setIssuer(ISSUER_INFO)
+		String token = Jwts.builder().setIssuedAt(new Date())
 				.setSubject(((User)auth.getPrincipal()).getUsername())
 				.setExpiration(new Date(System.currentTimeMillis() + TOKEN_EXPIRATION_TIME))
 				.signWith(SignatureAlgorithm.HS512, SUPER_SECRET_KEY).compact();
--- backedliststudy/src/main/java/com/liststudy/backedliststudy/user/UserWSImpl.java
+++ backedliststudy/src/main/java/com/liststudy/backedliststudy/user/UserWSImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,19 +30,25 @@ public class UserWSImpl {
 	}
 	
 	@PostMapping("/users")
-	public void saveUsuario(@RequestBody User user) {
+	public void createUser(@RequestBody User user) {
 		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		userJpaRepository.save(user);
 	}
 	
 	@GetMapping("/users")
-	public ResponseEntity<List<User>> getAllUsuarios() {
+	public ResponseEntity<List<User>> getAllUsers() {
 		return new ResponseEntity<List<User>>(userJpaRepository.findAll(), HttpStatus.OK);
 	}
 
 	@GetMapping("/users/{username}")
-	public  ResponseEntity<User> getUsuario(@PathVariable String username) {
+	public  ResponseEntity<User> getUserByUsername(@PathVariable String username) {
 		return new ResponseEntity<User>(userJpaRepository.findByUsername(username), HttpStatus.OK);
 	}
+	@GetMapping("/user/")
+	public  ResponseEntity<User> getMyUser() {
+		String user = (String)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		return getUserByUsername(user);
+	}
+	
 	
 }
