d3fe82f8a79b725c5de1b5d6f24a6d220ebcf59e
alber <alberto.laguna@alumnos.uva.es>
2019-02-21 19:34:36 +0100

    add LockModeType --> Optimist

--- backedliststudy/src/main/java/com/liststudy/backendliststudy/task/TaskServiceImpl.java
+++ backedliststudy/src/main/java/com/liststudy/backendliststudy/task/TaskServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.LockModeType;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -70,7 +71,7 @@ public class TaskServiceImpl  {
 		EntityTransaction transaction=em.getTransaction();
 		transaction.begin();
 		try {
-		Task task=em.find(Task.class, id);
+		Task task=em.find(Task.class, id,LockModeType.OPTIMISTIC);
 		if(task.getResolver()!=null) {
 			return false;
 		}
